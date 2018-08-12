e11e1ebcf191eeedac2c4844a42f090183c71e43
Beau Bennett <beau.bennett@beauknowssoftware.com>
2018-08-12 15:12:08 -0400

    Build in go path

--- ci/scripts/go-build.sh
+++ ci/scripts/go-build.sh
@@ -2,11 +2,17 @@
 set -e
 
 : ${SOURCE?Need a value}
+: ${CODE_DIR?Need a value}
 
 BASE=$(pwd -P)
 SOURCE="$BASE"/"$SOURCE"
+PACKAGE_PATH="$GOPATH"/src/github.com/terraform-providers/terraform-provider-aws
 
-cd "$SOURCE"
+mkdir -p "$PACKAGE_PATH"
+
+cp -r "$SOURCE" "$PACKAGE_PATH"
+
+cd "$PACKAGE_PATH"
 
 echo "Building"
 make build
